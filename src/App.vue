0b10e0fb77298a71e36f5aa43b87e7ce61aaece6
simone-nolberto <simone.nolberto@outlook.com>
2024-04-03 14:35:19 +0200

    update with working research function

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'Boolflix',
+  data(){
+    return{
+
+    }
+  },
+  components: {
+    AppHeader,
+  }
+}
 
 </script>
 
 <template>
+
+  <AppHeader></AppHeader>
   
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,35 @@
 <script>
+import { state } from '../state'
+
 export default {
-  name: 'AppHeader'
+    name: 'AppHeader',
+    data() {
+        return {
+            state,
+            userInput: '',  
+    }
+    },
+    methods:{
+        logInput(){
+            console.log(this.userInput);
+        },
+
+        findFilm(){
+            console.log(this.state.apiUrl + this.userInput );
+            this.state.getFilm(this.state.apiUrl + this.userInput)
+        }
+
+    }
 }
 </script>
 
 <template>
-  
+
+    <img src="" alt="">
+
+    <input type="text" name="" id="" v-model="userInput">
+    <button @click="findFilm()">Cerca</button>
+
 </template>
 
-<style>
-
-</style>
+<style></style>
